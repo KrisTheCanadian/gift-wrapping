9f7e45aea95fe4a389fda4f0d7c65fee8f89cfa5
KrisTheCanadian <kristhecanadian101@gmail.com>
2019-12-22 21:57:23 -0500

    points added

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+const points = [];
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	for(let i = 0; i < 10; i++)
+	{
+		points.push(createVector(random(width), random(height)));
+	}
 }
 
 function draw() {
+	background(0);
+	stroke(255);
+	strokeWeight(8);
 
+	for(let p of points)
+	{
+		point(p.x,p.y)
+	}
 }
